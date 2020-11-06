c479ef4c2de12ff0f3a6a6bad3843f51f42acafd
Melva Lang <mjlang74@gmail.com>
2020-11-05 20:04:14 -0600

    Created About page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,23 +4,41 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models.SchoolViewModels;
 using ContosoUniversity.Models;
 
 namespace ContosoUniversity.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        
+        private readonly SchoolContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(SchoolContext context)
         {
-            _logger = logger;
+            _context = context;
         }
+        
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         public IActionResult Privacy()
